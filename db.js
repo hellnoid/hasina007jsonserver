2fb8abe50a21e578e0f23c4f48ffa8f8324d0086
hellnoid <xainhellnoid22@gmail.com>
2022-08-11 00:18:09 +0530

    heroku1

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const items = require("./data/items");
 const tags = require("./data/tags");
 const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
+const cosmetics = require("./data/cosmetics");
 
 module.exports = {
   items,
   tags,
   brands,
-  itemTypes
+  itemTypes,
+  cosmetics
 }
