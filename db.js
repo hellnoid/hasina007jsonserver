568f8cac20fcef2426402620aac5ca3105344d51
hellnoid <xainhellnoid22@gmail.com>
2022-08-11 00:06:08 +0530

    heroku1

--- db.js
+++ db.js
@@ -9,28 +9,3 @@ module.exports = {
   brands,
   itemTypes
 }
-
-
-// const items = require("./data/items");
-
-// module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
- 
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
-
-//   return {
-//     items,
-//     tags,
-//     brands,
-//     itemTypes,
-//   };
-// };
