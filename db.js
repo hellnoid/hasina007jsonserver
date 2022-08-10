01d38c3ad3175cd654ce2240ce501995751385ab
hellnoid <xainhellnoid22@gmail.com>
2022-08-11 00:44:09 +0530

    heroku1

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const items = require("./data/items");
 const tags = require("./data/tags");
 const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
-const cosmetics = require("./data/cosmetics.json");
+const cosmetics = require("./data/cosmetics");
 
 module.exports = {
   items,
